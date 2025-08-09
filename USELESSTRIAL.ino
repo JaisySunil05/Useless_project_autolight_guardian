8368a0c743cd285fca51eb363822635a0a1f59fe
Hrisheekesh2005 <hrisheekeshnarayan@gmail.com>
2025-08-09 14:53:50 +0530

    audio code

--- USELESSTRIAL.ino
+++ USELESSTRIAL.ino
@@ -1,42 +1,67 @@
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
 
-// Pin Definitions
-#define SENSOR_OUTSIDE 2
-#define SENSOR_INSIDE 3
-#define LIGHT_SWITCH_PIN 4
-#define LIGHT_OUTPUT_PIN 5
+SoftwareSerial mp3Serial(10, 11); // DFPlayer TX, RX
+DFRobotDFPlayerMini mp3;
 
-// DFPlayer connections
-#define DF_TX 10  // Arduino TX to DFPlayer RX
-#define DF_RX 11  // Arduino RX to DFPlayer TX
+const int irInsidePin = 2;
+const int irOutsidePin = 3;
+const int lightSwitchPin = 4; // Switch input
+const int lightLEDPin = 5;    // LED output
 
-// Variables
 int peopleCount = 0;
-bool lightState = false;
-
-SoftwareSerial mySerial(DF_RX, DF_TX);
-DFRobotDFPlayerMini myDFPlayer;
+bool irInsideState = false;
+bool irOutsideState = false;
 
 void setup() {
-  pinMode(SENSOR_OUTSIDE, INPUT);
-  pinMode(SENSOR_INSIDE, INPUT);
-  pinMode(LIGHT_SWITCH_PIN, INPUT_PULLUP);
-  pinMode(LIGHT_OUTPUT_PIN, OUTPUT);
-
   Serial.begin(9600);
-  mySerial.begin(9600);
+  mp3Serial.begin(9600);
 
-  if (!myDFPlayer.begin(mySerial)) {
+  pinMode(irInsidePin, INPUT);
+  pinMode(irOutsidePin, INPUT);
+  pinMode(lightSwitchPin, INPUT_PULLUP); // switch
+  pinMode(lightLEDPin, OUTPUT);          // LED
+
+  if (!mp3.begin(mp3Serial)) {
     Serial.println("DFPlayer Mini not detected!");
     while (true);
   }
-  myDFPlayer.volume(25); // Set volume (0-30)
-
-  Serial.println("System Ready");
+  mp3.volume(25);
 }
 
 void loop() {
-  // Read switch state
-  lightState = digitalRead(LIGHT_SWITCH_PIN) == LOW; // LOW if switch ON
-  digitalWrite(LIGHT_OUTPUT_PIN, lightState);
+  // Read switch (LOW = ON)
+  bool lightOn = (digitalRead(lightSwitchPin) == LOW);
+
+  // Control LED based on switch
+  digitalWrite(lightLEDPin, lightOn ? HIGH : LOW);
+
+  // IR Sensor logic
+  if (digitalRead(irOutsidePin) == LOW) irOutsideState = true;
+  if (digitalRead(irInsidePin) == LOW) irInsideState = true;
+
+  // Person entering
+  if (irOutsideState && irInsideState) {
+    peopleCount++;
+    Serial.println("Person entered");
+    irOutsideState = false;
+    irInsideState = false;
+    delay(500);
+  }
+
+  // Person exiting
+  if (irInsideState && irOutsideState) {
+    peopleCount--;
+    if (peopleCount < 0) peopleCount = 0;
+    Serial.println("Person exited");
+    irInsideState = false;
+    irOutsideState = false;
+    delay(500);
+  }
+
+  // If room empty & light is ON → play audio
+  if (peopleCount == 0 && lightOn) {
+    mp3.play(1); // Play first audio file
+    delay(5000); // avoid repeated plays
+  }
+}
